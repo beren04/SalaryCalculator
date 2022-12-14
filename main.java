fedbc6c31b8489ca35426f170709073426386524
Beren <111902297+beren04@users.noreply.github.com>
2022-12-15 00:48:39 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package employee;
+
+public class info {
+    public static void main(String[] args) {
+        employee employe = new employee("beren",2000,45,1985);
+        String m= employe.toString();
+        System.out.println(m);
+    }
+}
